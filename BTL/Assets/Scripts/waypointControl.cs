236119661a073b4a702c5202e09c30bebd0cd9f3
bakaho <cleariqd@gmail.com>
2019-05-01 16:49:51 +0800

    waypointBasic

---
+++ BTL/Assets/Scripts/waypointControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointControl : MonoBehaviour {
+    public int myModule = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //update to firebase (name,times,which round)
+}
