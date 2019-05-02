9f9ddc67416ae8270f96fc9b55dd7b1501be0839
bakaho <cleariqd@gmail.com>
2019-05-02 15:51:54 +0800

    prepareFatch

--- BTL/Assets/Scripts/sqlDatabase.cs
+++ BTL/Assets/Scripts/sqlDatabase.cs
@@ -6,14 +6,27 @@ using Mono.Data.Sqlite;
 using System.IO;
 
 public class sqlDatabase : MonoBehaviour {
+    string dbPath;
+    IDbConnection dbcon;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //set a path and create the database
+        dbPath = "URI=file:" + Application.persistentDataPath + "/btlLocal.db";
+        dbcon = new SqliteConnection(dbPath);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void openDB(){
+        dbcon.Open();
+    }
+
+    public void createTable(){
+        IDbCommand dbcmd;
+        //dbcmd = 
+    }
 }
