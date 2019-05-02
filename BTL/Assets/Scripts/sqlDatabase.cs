d2150f5a2a124843c82fecdbe651158f52ec6856
bakaho <cleariqd@gmail.com>
2019-05-02 13:39:36 +0800

    sqlFilesUpdate

---
+++ BTL/Assets/Scripts/sqlDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class sqlDatabase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
