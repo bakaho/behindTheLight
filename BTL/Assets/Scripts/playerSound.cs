8ca9f5b0d8d46f1f99174bdc763de6ce7986dab3
bakaho <cleariqd@gmail.com>
2019-05-09 06:22:34 +0800

    AddSoundEfx

---
+++ BTL/Assets/Scripts/playerSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSound : MonoBehaviour {
+    [Header("Sound")]
+    AudioSource[] sources;
+    AudioClip[] clips;
+
+
+
+    void OnTriggerEnter(Collider mySound)
+    {
+        if (!GameManager.isPaused)
+        {
+            if (mySound.gameObject.CompareTag("soundEffect"))
+            {
+                if(!mySound.GetComponent<soundEffectControl>().isPlayed){
+                    mySound.GetComponent<AudioSource>().PlayOneShot(mySound.GetComponent<soundEffectControl>().myClip);
+                }
+            }
+        }
+    }
+}
---
+++ BTL/Assets/Scripts/soundEffectControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundEffectControl : MonoBehaviour {
+
+    public int myIndex;
+    public bool isPlayed;
+    public AudioClip myClip;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
